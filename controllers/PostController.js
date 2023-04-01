a2e92ee23d151875f11e066d2760bdacfd34c8f0
bonchukovIvan <bonchukooov@gmail.com>
2023-04-01 23:59:26 +0300

    add update and delete route

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -73,33 +73,65 @@ export const create = async (req, res) => {
 
 export const update = async (req, res) => {
     try {
-        const doc = new PostModel({
+        const postId = req.params.id;
+
+        const post = await PostModel.findOneAndUpdate({
+            _id: postId,
+        },
+        {
             title: req.body.title,
             text: req.body.text,
             imageUrl: req.body.imageUrl,
             tags: req.body.tags,
             user: req.userId,
+        })
+        .then(() => {
+            res.json({
+                success: true,
+            });
+        })
+        .catch((err) => {
+            res.status(404).json({
+                message: 'Не удалось обновить статью',
+            });
         });
-
-        const post = await doc.save();
         
-        res.json(post);
 
     } catch (err) {
         res.status(500).json({
-            message: 'Не удалось создать пост',
+            message: 'Не удалось обновить статью',
         });
     }
 }
 
-export const deletePost = async (req, res) => {
+export const remove = async (req, res) => {
     try {
         const postId = req.params.id;
-        const posts = await PostModel.deleteOne(postId);
-        res.json(posts);
+
+        const post = await PostModel.findOneAndDelete(
+        {
+            _id: postId,
+        })
+        .then((doc) => {
+            if (!doc) {
+                return res.status(404).json({
+                    message: 'Статья не найдена',
+                });
+            }
+            res.json(doc);
+        })
+        .catch((err, doc) => {
+             if (err) {
+                return res.status(500).json({
+                    message: 'Не удалось удалить статью',
+                });
+            }
+        });
+
     } catch (err) {
+        console.log(err);
         res.status(500).json({
-            message: 'Не удалось создать пост',
+            message: 'Не удалось удалить статью статью',
         });
     }
 }
